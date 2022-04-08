b4470cbe6d104f575a8d92bcf26a5facd6b32171
Nicolas Bologna <nicolasandresbologna@hotmail.com>
2022-04-07 22:29:16 -0300

    Clase 2

--- Linq/Ej3.cs
+++ Linq/Ej3.cs
@@ -4,7 +4,16 @@
     {
         public Ej3()
         {
+            List<string> list = new List<string>();
+            list.Add("computadora");
+            list.Add("usb");
 
+            List<string>? listaNueva = list.Where(palabra => palabra.Length >= 5).Select(palabra => palabra.ToUpper()).ToList();
+
+            foreach (var item in listaNueva)
+            {
+                Console.WriteLine(item);
+            }
         }
         public static List<string> BuscarPalabrasPorLetras(List<string> palabras)
         {
--- Linq/Program.cs
+++ Linq/Program.cs
@@ -4,7 +4,7 @@
     {
         public static void Main()
         {
-            Ej7 ej = new();
+            Ej3 ej = new();
         }
     }
 }
