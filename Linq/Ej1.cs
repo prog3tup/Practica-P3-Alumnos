dcd5eb2601f872c5e7dbbed1acfb95e3ea96b515
brunofruet <brunofruet17@gmail.com>
2022-04-15 14:20:05 -0300

    cambios merge

--- Linq/Ej1.cs
+++ Linq/Ej1.cs
@@ -6,7 +6,20 @@ Ejemplo: [67, 92, 153, 15] → 67, 92
 
 namespace PracticaLinq
 {
-    class Ej1
+    public class Ej1
     {
-    }
+        public Ej1()
+        {
+
+            var listEj = new List<int>() { 67, 92, 153, 15 };
+
+            var filterList = listEj.Where(n => n > 30 && n < 100);
+
+            foreach (var item in filterList)
+            {
+                Console.WriteLine(item);
+            }
+
+        }
+}
 }
--- Linq/Ej2.cs
+++ Linq/Ej2.cs
@@ -5,8 +5,18 @@ Ejemplo: "computadora", "usb" → "COMPUTADORA"
 
  */
 namespace PracticaLinq
-{
-    internal class Ej2
+    public class Ej2
     {
-    }
-}
+        public Ej2()
+        {
+
+            var listEj = new List<string>() { "usb", "computadora", "mouse", "mic", "teclado"};
+
+            var filterList = listEj.Where( word => word.Length >= 5);
+
+            foreach (var item in filterList)
+            {
+                Console.WriteLine(item.ToUpper());
+            }
+
+        }
